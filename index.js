97ae5b88745bc459f52ea7c27787da67ef3f52d6
Aerodos12 <weldon.henson97@gmail.com>
2017-06-17 16:58:29 -0400

    EM2 Bot Rewrite

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const Discord = require('discord.js')'
+const Discord = require('discord.js');
+const Express4 = require('express.js');
 const EM2Client = new Discord.Client();
+const EM2ClientWeb = express();
+app.set('view engine', 'pug')
+app.get('/',function(req,res){
+	  res.render('index');
+});
 client.on("ready", () => {
    
 });
-client.login("MzI1NzI1Nzc4MDMxNDExMjAx.DCcbUg.V-bT5skqcdfyBZEid9Bream7ce0")
+client.login("MzI1NzI1Nzc4MDMxNDExMjAx.DCcbUg.V-bT5skqcdfyBZEid9Bream7ce0");
+app.listen(5000, function() {
+	console.log("Started the EM2 Bot Center.");
+});
