92b9d81394ddb8f03d2653107cb01693001b9551
Aerodos12 <weldon.henson97@gmail.com>
2017-06-17 18:50:42 -0400

    EM2 Bot Rewrite

--- core.py
+++ core.py
@@ -1,15 +1,25 @@
 import discord
 import asyncio
+from discord.ext import commands
+import squadFunctionsHelp
 
-client = discord.Client()
+description = '''The official EM2 Bot and the official Discord Bot for Saberfront: Alderaanian Assault 3
+The main set of comands that can be used here are for the new UMS Squad System.
+'''
+EM2Server = commands.Bot(command_prefix='em2:', description=description)
 
-@client.event
+@EM2Server.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
+    
+@EM2Server.command()
+async def squadFunctionInfo(squadFunc : str):
+    """Helps you understand what a specified UMS Squad Function means."""
+    await EM2Server.say(squadFunctionsHelp.getSquadFunction(squadFunc))
 
 
 client.run('MzI1NzI1Nzc4MDMxNDExMjAx.DCc2_Q.o3z3f-Msx8I_uO8YeN-b7AL2hG0')
---
+++ squadFunctionsHelp.py
@@ -0,0 +1,8 @@
+
+
+squadFunctions =  { "*squad" : "Refers to the whole squad"}
+
+def getSquadFunction(funcName):
+   return squadFunctions[funcName]
+
+
