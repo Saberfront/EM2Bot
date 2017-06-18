521fa3e86b848a1dd34b0c38064722ec5e6e01a1
Aerodos12 <weldon.henson97@gmail.com>
2017-06-18 15:54:08 -0400

    EM2 Bot Rewrite

--- squadFunctionsHelp.py
+++ squadFunctionsHelp.py
@@ -7,9 +7,11 @@ squadFunctions =  {
 "nco" : "Refers to Non-commissioned officers (Rebel troopers and Stormtroopers)",
 "friend" : "Refers to you squad leader friends (players)",
 "%" : "Refers to ANY personnel that are on your side (players, Player Soldiers (Squad Members), Pilots, etc.)",
-
-
-
+"squad-" : "Refers to any personnel within the specified squad id",
+"faction-" : "Refers to any personnel whose squad leaders are part of the specified class",
+"-" : "Refers to any squad leader who is NOT the one with the specified Roblox User ID",
+"#" : "Refers to a randomly selected group of squad members from your squad, limited by the number of members you specify",
+"radius-" : "Refers to members of your squad who are within a specified distance of you, REGARDLESS of direction",
 
 }
 
