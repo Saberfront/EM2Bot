4c7e679f005abd15fb57f98f8709f6412b2a2c04
Aerodos12 <weldon.henson97@gmail.com>
2017-06-18 15:35:29 -0400

    EM2 Bot Rewrite

--- squadFunctionsHelp.py
+++ squadFunctionsHelp.py
@@ -6,6 +6,10 @@ squadFunctions =  {
 "@allsquads" : "Refers to every player and their squadmates (the whole server)",
 "nco" : "Refers to Non-commissioned officers (Rebel troopers and Stormtroopers)",
 "friend" : "Refers to you squad leader friends (players)",
+"%" : "Refers to ANY personnel that are on your side (players, Player Soldiers (Squad Members), Pilots, etc.)",
+
+
+
 
 }
 
