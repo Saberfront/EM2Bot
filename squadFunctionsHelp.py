5066cd0cdd378533d2f20d529c460d80335ef358
Aerodos12 <weldon.henson97@gmail.com>
2017-06-18 08:36:13 -0400

    EM2 Bot Rewrite

--- squadFunctionsHelp.py
+++ squadFunctionsHelp.py
@@ -3,7 +3,10 @@
 squadFunctions =  { 
 "squad" : "Refers to the whole squad",
 "prime" : "Refers to the first member of your squad",
-"@allsquads" : "Refer to every player and their squadmates (the whole server)",
+"@allsquads" : "Refers to every player and their squadmates (the whole server)",
+"nco" : "Refers to Non-commissioned officers (Rebel troopers and Stormtroopers)",
+"friend" : "Refers to you squad leader friends (players)",
+
 }
 
 def getSquadFunction(funcName):
