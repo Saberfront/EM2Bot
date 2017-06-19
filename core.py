217e0ec4f43e13aa408902f011d8f8d7df90dc64
Aerodos12 <weldon.henson97@gmail.com>
2017-06-19 12:34:54 -0400

    Mob Blasters

--- core.py
+++ core.py
@@ -2,7 +2,7 @@ import discord
 import asyncio
 from discord.ext import commands
 import squadFunctionsHelp
-
+import mobItems
 description = '''The official EM2 Bot and the official Discord Bot for Saberfront: Alderaanian Assault 3
 The main set of commands that can be used here are for the new UMS Squad System AND Extraction Mode 2.
 '''
@@ -21,5 +21,9 @@ async def squadFunctionInfo(squadFunc : str):
     """Helps you understand what a specified UMS Squad Function means."""
     await EM2Server.say(squadFunctionsHelp.getSquadFunction(squadFunc))
 
+@EM2Server.command()
+async def availableSquadBlasters():
+    """ Shows a list of blasters available for Player Soldiers (Mobs) to use. """
+    await EM2Server.say(mobItems.getBlasterString())
 
 EM2Server.run('MzI1NzI1Nzc4MDMxNDExMjAx.DCc2_Q.o3z3f-Msx8I_uO8YeN-b7AL2hG0')
---
+++ mobItems.py
@@ -0,0 +1,11 @@
+
+availableBlasters = {
+    "A280",
+    "A280C",
+    "DC15X",
+    "DT29",
+    "E-11",
+}
+
+def getBlasterString():
+    return """ **AVAILABLE BLASTERS**: \n ```1. A280 \n 2. A280C \n 3. DC15X \n 4. DT29 \n 5. E-11 \n ``` """
