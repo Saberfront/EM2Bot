9adc1126fe4a9e6847f003c3cb96d2bad26fd321
Aerodos12 <weldon.henson97@gmail.com>
2017-06-19 19:47:35 -0400

    Django

--- mobItems.py
+++ mobItems.py
@@ -1,12 +1,12 @@
 
-availableBlasters = {
+availableBlasters = [
     "A280",
     "A280C",
     "A380",
     "DC15X",
     "DT29",
     "E-11",
-}
+]
 
 def getBlasterString():
     blasterStr = """**AVAILABLE BLASTERS**: \n ```"""
