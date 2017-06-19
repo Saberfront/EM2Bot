c83a6df4b50a567f02ff74c7b70eed0ab44122ed
Aerodos12 <weldon.henson97@gmail.com>
2017-06-19 14:11:51 -0400

    Django

--- botSettings/models.py
+++ botSettings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class IntSetting(models.Model):
+    key = models.CharField(max_length=255)
+    value = models.IntegerField(default=0)
+    date_modified = models.DateTimeField('date modified')
+    
